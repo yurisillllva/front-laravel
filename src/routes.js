5309cfc4fd0a5279dbaf0cfc161d83a9f5c7359e
Yuri Silva <ysm281@gmail.com>
2023-02-16 01:34:23 -0300

    first commit

---
+++ src/routes.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Logon from "src/pages/Logon";
+
+export default function Routes() {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact component={Logon} />
+      </Switch>
+    </BrowserRouter>
+  );
+}
